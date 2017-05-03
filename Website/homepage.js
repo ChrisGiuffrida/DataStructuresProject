fab7bbab087ffcdae95d1ba8b642f23d226cd970
ChrisGiuffrida <chris@gonotredame.net>
2017-05-03 10:12:28 -0400

    Updates

---
+++ Website/homepage.js
@@ -0,0 +1,6 @@
+$('help-button').click(function () {      
+    $('.parent').append("<div class='alert alert-warning alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Enter a phrase five words in length or less.</div>")
+
+	})
+
+});
