d8d359df0007bed1045587e939156837af3e37f4
Chris Giuffrida <chris@gonotredame.net>
2017-05-05 09:34:22 -0400

    Delete homepage.js

--- Website/homepage.js
+++
@@ -1,6 +0,0 @@
-$('help-button').click(function () {      
-    $('.parent').append("<div class='alert alert-warning alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Enter a phrase five words in length or less.</div>")
-
-	})
-
-});
